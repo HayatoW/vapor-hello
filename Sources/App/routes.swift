face6231d45c3241ef2e04c2602146f5d3a43669
Hayato Watanabe <solideye@me.com>
2020-11-29 18:32:54 +0900

    Update routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,20 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+
+    app.get("galaxies") { req in
+        Galaxy.query(on: req.db).with(\.$stars).all()
+    }
+    
+    app.post("galaxies") { req -> EventLoopFuture<Galaxy> in
+        let galaxy = try req.content.decode(Galaxy.self)
+        return galaxy.create(on: req.db)
+            .map { galaxy }
+    }
+    
+    app.post("stars") { req -> EventLoopFuture<Star> in
+        let star = try req.content.decode(Star.self)
+        return star.create(on: req.db)
+            .map { star }
+    }
 }
